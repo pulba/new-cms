0b7facbca912a75a600c363a5faaa0860adcd280
pulba <pulbagrup@gmail.com>
2026-05-21 10:06:21 +0700

    fix: disable Astro sessions, remove invalid KV placeholder, add imageService passthrough

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    sessions: false,
+  }),
   integrations: [
     react(),
     tailwind({
